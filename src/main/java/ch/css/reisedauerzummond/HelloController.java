a4e7c58f700649e92253397f6c067238d470b300
NoeFarese <noefarese@gmx.ch>
2022-12-02 12:01:54 +0100

    push 1

--- src/main/java/ch/css/reisedauerzummond/HelloController.java
+++ src/main/java/ch/css/reisedauerzummond/HelloController.java
@@ -1,6 +1,7 @@
 package ch.css.reisedauerzummond;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
@@ -21,17 +22,42 @@ public class HelloController {
 
     @FXML
     private void ButtonLogic(){
-        double speedKmh = convertToInt64(textFieldGeschwindigkeit.getText());
-        double result = 0.0;
+        double speedKmh = convertToDouble(textFieldGeschwindigkeit.getText());
+        double resultat = 0.0;
 
         Mondreise mondreise = new Mondreise(speedKmh);
 
         if (radioButtonTage.isSelected() == true) {
-            result = mondreise.getTravelDurationDays();
+            resultat = mondreise.getTravelDurationDays();
         }
         else {
-            result = mondreise.getTravelDurationHours();
+            resultat = mondreise.getTravelDurationHours();
         }
+    }
+
+    private double convertToDouble(String value) {
+        try {
+            double result = Double.parseDouble(value);
+
+            return result;
+        }
+        catch(NumberFormatException ex) {
+            showExceptionMessageBox("Bitte geben Sie eine Zahl ein");
+            return 0;
+        }
+    }
+
+    private void showExceptionMessageBox(String fehlerAusgabe) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Fehler");
+        alert.setHeaderText("Ein Fehler ist aufgetreten.");
+        alert.setContentText(fehlerAusgabe);
+        alert.showAndWait();
+    }
+
+    @FXML
+    private void initialize() {
+        textFieldGeschwindigkeit.isFocused();
     }
 }
 
